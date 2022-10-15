fad02bd3f32422b9092405d36337c2f4d56d42e2
ThomIves <thom.ives@gmail.com>
2022-10-14 21:43:10 -0600

    trying to control ops on workstation jenkins install 5.

--- src/main.py
+++ src/main.py
@@ -5,4 +5,4 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hey - Being More specific with ops on local machine using repo."}
+    return {"message": "THIS SHOULD WORK!"}
